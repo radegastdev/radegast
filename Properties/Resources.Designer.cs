b58eeb2f9a22dd5532af780548a1195fa1c28db6
Latif Khalifa <latifer@streamgrid.net>
2009-06-13 18:11:08 +0000

    On to 1.6 ...
    git-svn-id: https://radegast.googlecode.com/svn/trunk/Radegast@33 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -511,7 +511,7 @@ namespace Radegast.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Radegast 1.5.
+        ///   Looks up a localized string similar to Radegast 1.6.
         /// </summary>
         public static string RadegastTitle {
             get {
