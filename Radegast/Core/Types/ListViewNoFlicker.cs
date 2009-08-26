04c3473d79be32545f50abe5d826c6f40777442a
Latif Khalifa <latifer@streamgrid.net>
2009-08-26 22:21:42 +0000

    Remove cruft.
    git-svn-id: https://radegast.googlecode.com/svn/trunk@163 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/Core/Types/ListViewNoFlicker.cs
+++ Radegast/Core/Types/ListViewNoFlicker.cs
@@ -29,9 +29,6 @@
 // $Id$
 //
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Radegast
@@ -57,74 +54,5 @@ namespace Radegast
                 base.OnNotifyMessage(m);
             }
         }
-
     }
-
-    public class ListViewItemComparer : IComparer<object>
-    {
-        // Initialize the variables to default
-        public int column = 0;
-        public bool bAscending = true;
-
-        // Using the Compare function of IComparer
-        public int Compare(object x, object y)
-        {
-            // Cast the objects to ListViewItems
-            ListViewItem lvi1 = (ListViewItem)x;
-            ListViewItem lvi2 = (ListViewItem)y;
-
-            // If the column is the string columns
-            if (column != 2)
-            {
-                string lvi1String = lvi1.SubItems[column].ToString();
-                string lvi2String = lvi2.SubItems[column].ToString();
-
-                // Return the normal Compare
-                if (bAscending)
-                    return String.Compare(lvi1String, lvi2String);
-
-                // Return the negated Compare
-                return -String.Compare(lvi1String, lvi2String);
-            }
-
-            // The column is the Age column
-            int lvi1Int = ParseListItemString(lvi1.SubItems[column].ToString());
-            int lvi2Int = ParseListItemString(lvi2.SubItems[column].ToString());
-
-            // Return the normal compare.. if x < y then return -1
-            if (bAscending)
-            {
-                if (lvi1Int < lvi2Int)
-                    return -1;
-                else if (lvi1Int == lvi2Int)
-                    return 0;
-
-                return 1;
-            }
-
-            // Return the opposites for descending
-            if (lvi1Int > lvi2Int)
-                return -1;
-            else if (lvi1Int == lvi2Int)
-                return 0;
-
-            return 1;
-        }
-
-        private int ParseListItemString(string x)
-        {
-            //ListViewItems are returned like this: "ListViewSubItem: {19}"
-            int counter = 0;
-            for (int i = x.Length - 1; i >= 0; i--, counter++)
-            {
-                if (x[i] == '{')
-                    break;
-            }
-
-            return Int32.Parse(x.Substring(x.Length - counter, counter - 1));
-        }
-    }
-
-
-
 }
