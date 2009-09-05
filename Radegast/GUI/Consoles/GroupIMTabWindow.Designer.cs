6c4ec8e462c424910a6caac97ff8513781ac75a9
Latif Khalifa <latifer@streamgrid.net>
2009-09-05 04:05:49 +0000

    RAD-3: Added group chat participant list
    git-svn-id: https://radegast.googlecode.com/svn/trunk@194 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/GUI/Consoles/GroupIMTabWindow.Designer.cs
+++ Radegast/GUI/Consoles/GroupIMTabWindow.Designer.cs
@@ -61,20 +61,24 @@ namespace Radegast
             this.rtbIMText = new System.Windows.Forms.RichTextBox();
             this.cbxInput = new System.Windows.Forms.ComboBox();
             this.btnSend = new System.Windows.Forms.Button();
+            this.chatSplit = new System.Windows.Forms.SplitContainer();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.Participants = new Radegast.ListViewNoFlicker();
+            this.chatSplit.Panel1.SuspendLayout();
+            this.chatSplit.Panel2.SuspendLayout();
+            this.chatSplit.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbIMText
             // 
-            this.rtbIMText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.rtbIMText.BackColor = System.Drawing.Color.White;
+            this.rtbIMText.Dock = System.Windows.Forms.DockStyle.Fill;
             this.rtbIMText.HideSelection = false;
-            this.rtbIMText.Location = new System.Drawing.Point(3, 3);
+            this.rtbIMText.Location = new System.Drawing.Point(0, 0);
             this.rtbIMText.Name = "rtbIMText";
             this.rtbIMText.ReadOnly = true;
-            this.rtbIMText.Size = new System.Drawing.Size(494, 295);
-            this.rtbIMText.TabIndex = 2;
+            this.rtbIMText.Size = new System.Drawing.Size(373, 302);
+            this.rtbIMText.TabIndex = 3;
             this.rtbIMText.Text = "";
             this.rtbIMText.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtbIMText_LinkClicked);
             // 
@@ -85,7 +89,7 @@ namespace Radegast
             this.cbxInput.FormattingEnabled = true;
             this.cbxInput.Location = new System.Drawing.Point(3, 306);
             this.cbxInput.Name = "cbxInput";
-            this.cbxInput.Size = new System.Drawing.Size(413, 21);
+            this.cbxInput.Size = new System.Drawing.Size(373, 21);
             this.cbxInput.TabIndex = 0;
             this.cbxInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.cbxInput_KeyUp);
             this.cbxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbxInput_KeyDown);
@@ -95,24 +99,74 @@ namespace Radegast
             // 
             this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSend.Enabled = false;
-            this.btnSend.Location = new System.Drawing.Point(422, 304);
+            this.btnSend.Location = new System.Drawing.Point(380, 305);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(55, 23);
             this.btnSend.TabIndex = 1;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
+            // chatSplit
+            // 
+            this.chatSplit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.chatSplit.Location = new System.Drawing.Point(3, 3);
+            this.chatSplit.Name = "chatSplit";
+            // 
+            // chatSplit.Panel1
+            // 
+            this.chatSplit.Panel1.Controls.Add(this.rtbIMText);
+            // 
+            // chatSplit.Panel2
+            // 
+            this.chatSplit.Panel2.Controls.Add(this.Participants);
+            this.chatSplit.Size = new System.Drawing.Size(494, 302);
+            this.chatSplit.SplitterDistance = 373;
+            this.chatSplit.TabIndex = 3;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShow.Location = new System.Drawing.Point(442, 305);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(55, 23);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Participants
+            // 
+            this.Participants.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.Participants.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Participants.HideSelection = false;
+            this.Participants.Location = new System.Drawing.Point(0, 0);
+            this.Participants.MultiSelect = false;
+            this.Participants.Name = "Participants";
+            this.Participants.ShowGroups = false;
+            this.Participants.Size = new System.Drawing.Size(117, 302);
+            this.Participants.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.Participants.TabIndex = 4;
+            this.Participants.UseCompatibleStateImageBehavior = false;
+            this.Participants.View = System.Windows.Forms.View.List;
+            this.Participants.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Participants_MouseDoubleClick);
+            // 
             // GroupIMTabWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chatSplit);
             this.Controls.Add(this.cbxInput);
-            this.Controls.Add(this.rtbIMText);
+            this.Controls.Add(this.btnShow);
             this.Controls.Add(this.btnSend);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "GroupIMTabWindow";
             this.Size = new System.Drawing.Size(500, 330);
+            this.chatSplit.Panel1.ResumeLayout(false);
+            this.chatSplit.Panel2.ResumeLayout(false);
+            this.chatSplit.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -122,5 +176,8 @@ namespace Radegast
         private System.Windows.Forms.RichTextBox rtbIMText;
         private System.Windows.Forms.ComboBox cbxInput;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.SplitContainer chatSplit;
+        private ListViewNoFlicker Participants;
+        private System.Windows.Forms.Button btnShow;
     }
 }
--- Radegast/GUI/Consoles/GroupIMTabWindow.cs
+++ Radegast/GUI/Consoles/GroupIMTabWindow.cs
@@ -30,6 +30,7 @@
 //
 using System;
 using System.Drawing;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Threading;
 using Radegast.Netcom;
@@ -56,27 +57,112 @@ namespace Radegast
             this.session = session;
 
             textManager = new IMTextManager(this.instance, new RichTextBoxPrinter(rtbIMText), this.session, sessionName);
-            
+
+            btnShow.Text = "Show";
+            chatSplit.Panel2Collapsed = true;
+
             // Callbacks
             client.Self.OnGroupChatJoin += new AgentManager.GroupChatJoinedCallback(Self_OnGroupChatJoin);
-
+            client.Self.OnChatSessionMemberAdded += new AgentManager.ChatSessionMemberAddedCallback(Self_OnChatSessionMemberAdded);
+            client.Self.OnChatSessionMemberLeft += new AgentManager.ChatSessionMemberLeftCallback(Self_OnChatSessionMemberLeft);
+            client.Avatars.OnAvatarNames += new AvatarManager.AvatarNamesCallback(Avatars_OnAvatarNames);
             client.Self.RequestJoinGroupChat(session);
         }
 
         private void IMTabWindow_Disposed(object sender, EventArgs e)
         {
             client.Self.OnGroupChatJoin -= new AgentManager.GroupChatJoinedCallback(Self_OnGroupChatJoin);
+            client.Self.OnChatSessionMemberAdded -= new AgentManager.ChatSessionMemberAddedCallback(Self_OnChatSessionMemberAdded);
+            client.Self.OnChatSessionMemberLeft -= new AgentManager.ChatSessionMemberLeftCallback(Self_OnChatSessionMemberLeft);
+            client.Avatars.OnAvatarNames -= new AvatarManager.AvatarNamesCallback(Avatars_OnAvatarNames);
             CleanUp();
         }
 
-
-        void Self_OnGroupChatJoin(UUID groupChatSessionID, string sessionName, UUID tmpSessionID, bool success)
+        void Avatars_OnAvatarNames(Dictionary<UUID, string> names)
         {
-            if (groupChatSessionID != session && tmpSessionID != session) {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(() => Avatars_OnAvatarNames(names)));
                 return;
             }
 
-            if (InvokeRequired) {
+            Participants.BeginUpdate();
+
+            foreach (KeyValuePair<UUID, string> kvp in names)
+            {
+                if (Participants.Items.ContainsKey(kvp.Key.ToString()))
+                    Participants.Items[kvp.Key.ToString()].Text = kvp.Value;
+            }
+
+            Participants.Sort();
+            Participants.EndUpdate();
+        }
+
+        void Self_OnChatSessionMemberLeft(UUID sessionID, UUID agent_key)
+        {
+            if (sessionID == session)
+                UpdateParticipantList();
+        }
+
+        void Self_OnChatSessionMemberAdded(UUID sessionID, UUID agent_key)
+        {
+            if (sessionID == session)
+                UpdateParticipantList();
+        }
+
+        void UpdateParticipantList()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(UpdateParticipantList));
+                return;
+            }
+
+            Participants.BeginUpdate();
+            Participants.Items.Clear();
+
+            List<ChatSessionMember> participants;
+            List<UUID> nameLookup = new List<UUID>();
+
+            if (client.Self.GroupChatSessions.TryGetValue(session, out participants))
+            {
+                ChatSessionMember[] members = participants.ToArray();
+                for (int i = 0; i < members.Length; i++)
+                {
+                    ChatSessionMember participant = members[i];
+                    ListViewItem item = new ListViewItem();
+                    item.Name = participant.AvatarKey.ToString();
+                    if (instance.nameCache.ContainsKey(participant.AvatarKey))
+                    {
+                        item.Text = instance.nameCache[participant.AvatarKey];
+                    }
+                    else
+                    {
+                        item.Text = RadegastInstance.INCOMPLETE_NAME;
+                        nameLookup.Add(participant.AvatarKey);
+                    }
+                    if (participant.IsModerator)
+                        item.Font = new Font(item.Font, FontStyle.Bold);
+                    Participants.Items.Add(item);
+                }
+
+                if (nameLookup.Count > 0)
+                    client.Avatars.RequestAvatarNames(nameLookup);
+            }
+
+            Participants.Sort();
+            Participants.EndUpdate();
+        }
+
+        void Self_OnGroupChatJoin(UUID groupChatSessionID, string sessionName, UUID tmpSessionID, bool success)
+        {
+            if (groupChatSessionID != session && tmpSessionID != session)
+            {
+                return;
+            }
+
+            if (InvokeRequired)
+            {
                 Invoke(new MethodInvoker(
                     delegate()
                     {
@@ -86,10 +172,13 @@ namespace Radegast
                 );
                 return;
             }
-            if (success) {
+            if (success)
+            {
                 textManager.TextPrinter.PrintTextLine("Join Group Chat Success!", Color.Green);
                 WaitForSessionStart.Set();
-            } else {
+            }
+            else
+            {
                 textManager.TextPrinter.PrintTextLine("Join Group Chat failed.", Color.Red);
             }
         }
@@ -124,6 +213,20 @@ namespace Radegast
             this.ClearIMInput();
         }
 
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (chatSplit.Panel2Collapsed)
+            {
+                chatSplit.Panel2Collapsed = false;
+                btnShow.Text = "Hide";
+            }
+            else
+            {
+                chatSplit.Panel2Collapsed = true;
+                btnShow.Text = "Show";
+            }
+        }
+
         private void cbxInput_TextChanged(object sender, EventArgs e)
         {
             RefreshControls();
@@ -135,8 +238,13 @@ namespace Radegast
             {
                 cbxInput.Enabled = false;
                 btnSend.Enabled = false;
+                btnShow.Enabled = false;
+                btnShow.Text = "Show";
+                chatSplit.Panel2Collapsed = true;
                 return;
             }
+
+            btnShow.Enabled = true;
 
             if (cbxInput.Text.Length > 0)
             {
@@ -216,5 +324,19 @@ namespace Radegast
             get { return textManager; }
             set { textManager = value; }
         }
+
+        private void Participants_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListViewItem item = Participants.GetItemAt(e.X, e.Y);
+            if (item != null)
+            {
+                try
+                {
+                    new frmProfile(instance, item.Text, new UUID(item.Name)).Show();
+                }
+                catch (Exception) { }
+            }
+        }
+
     }
 }
