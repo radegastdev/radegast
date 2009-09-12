fc846a8959f60ebc09cc1c889c69af7e05d52f39
Latif Khalifa <latifer@streamgrid.net>
2009-09-12 17:39:49 +0000

    Added copyright headers Added some stuff that would be useful for classes that inherit RadegastCommand
    git-svn-id: https://radegast.googlecode.com/svn/trunk@224 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/Core/Commands/ThreadCommand.cs
+++ Radegast/Core/Commands/ThreadCommand.cs
@@ -1,3 +1,33 @@
+// 
+// Radegast Metaverse Client
+// Copyright (c) 2009, Radegast Development Team
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the application "Radegast", nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// $Id$
+//
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -15,7 +45,7 @@ namespace Radegast.Commands
 
         public string Description
         {
-            get { return "Runs a command in a thread";  }
+            get { return "Runs a command in a thread"; }
         }
 
         public string Usage
@@ -30,13 +60,13 @@ namespace Radegast.Commands
 
         public void StopCommand(RadegastInstance inst)
         {
-            foreach(var cmd in _commandThreads)
+            foreach (var cmd in _commandThreads)
             {
                 try
                 {
                     cmd.Abort();
                 }
-                catch(Exception)
+                catch (Exception)
                 {
                 }
             }
@@ -46,17 +76,17 @@ namespace Radegast.Commands
         public void Execute(string threadCmd, string[] cmdArgs, ConsoleWriteLine WriteLine)
         {
             string args = String.Join(" ", cmdArgs);
-            if (args==string.Empty)
+            if (args == string.Empty)
             {
                 lock (_commandThreads)
                 {
-                    if (_commandThreads.Count==0)
+                    if (_commandThreads.Count == 0)
                     {
-                        WriteLine("No threaded commands.");                       
+                        WriteLine("No threaded commands.");
                     }
                     else
                     {
-                        WriteLine("Threaded command list" );
+                        WriteLine("Threaded command list");
                         foreach (Thread thr in _commandThreads)
                         {
                             WriteLine(" * {0}", thr.Name);
@@ -79,7 +109,7 @@ namespace Radegast.Commands
                                                    WriteLine("Done with {0}", args);
                                                    _commandThreads.Remove(Thread.CurrentThread);
                                                }
-                                           }) {Name = "ThreadCommand for " + args};
+                                           }) { Name = "ThreadCommand for " + args };
             lock (_commandThreads) _commandThreads.Add(thread);
             thread.Start();
         }
