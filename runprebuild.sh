1275cbe1424d2d1806338fd3411b0fadd9fe16cd
Latif Khalifa <latifer@streamgrid.net>
2009-10-07 03:50:34 +0000

    Different path, no openjpeg and mark as executable
    git-svn-id: https://radegast.googlecode.com/svn/trunk@325 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- runprebuild.sh
+++ runprebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mono bin/Prebuild.exe /target nant
-mono bin/Prebuild.exe /target monodev
-mono bin/Prebuild.exe /target vs2008
+mono Radegast/Prebuild.exe /target nant
+mono Radegast/Prebuild.exe /target monodev
+mono Radegast/Prebuild.exe /target vs2008
 
 if [ x$1 == xnant ]; then
     nant -buildfile:Radegast.build
@@ -13,15 +13,4 @@ if [ x$1 == xnant ]; then
     fi
     
     exit $RES
-fi
-
-if [ x$1 == xopenjpeg ]; then
-   ARCH=`arch`
-   cd openjpeg-dotnet
-   if [ $ARCH == x86_64 ]; then
-      # since we're a 64bit host, compile a 32bit vesion of openjpeg
-      make ARCH=-i686 ARCHFLAGS=-m32 install
-   fi
-      # compile for default detected platform
-      make install
 fi
