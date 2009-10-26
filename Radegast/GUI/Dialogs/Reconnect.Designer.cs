9591870b268130acaace570cf878e0f46dc43dd2
Latif Khalifa <latifer@streamgrid.net>
2009-10-26 12:12:52 +0000

    Another minor tweak.
    git-svn-id: https://radegast.googlecode.com/svn/trunk@374 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/GUI/Dialogs/Reconnect.Designer.cs
+++ Radegast/GUI/Dialogs/Reconnect.Designer.cs
@@ -128,7 +128,7 @@ namespace Radegast
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(410, 117);
+            this.ClientSize = new System.Drawing.Size(410, 114);
             this.ControlBox = false;
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnDisable);
