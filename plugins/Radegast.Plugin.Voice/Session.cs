6468a0453cc9731c4efeff01c3930daa471db235
Mojito Sorbet <mojitotech@gmail.com>
2009-10-27 21:19:45 +0000

    First commit of new Voice plugin.
    git-svn-id: https://radegast.googlecode.com/svn/trunk@382 f7a694da-4d33-11de-9ad6-1127a62b9fcd

---
+++ plugins/Radegast.Plugin.Voice/Session.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenMetaverse;
+using System.Windows.Forms;
+using OpenMetaverse.Voice;
+using Radegast;
+
+namespace Radegast.Plugin.Voice
+{
+    internal class Session
+    {
+        private VoiceControl control;
+        internal string SessionHandle;
+        private static Dictionary<string, VoiceParticipant> knownParticipants;
+        private SessionForm sessionForm;
+        private string regionName;
+        internal bool IsSpatial { get; set; }
+        private VoiceGateway connector;
+
+        internal Session(VoiceControl pc, VoiceGateway conn, string handle)
+        {
+            control = pc;
+            SessionHandle = handle;
+            connector = conn;
+
+            IsSpatial = true;
+            knownParticipants = new Dictionary<string, VoiceParticipant>();
+
+            IAsyncResult creation = control.instance.MainForm.BeginInvoke(
+               new MethodInvoker(delegate()
+            {
+                sessionForm = new SessionForm(control, this);
+                sessionForm.Show();
+            }));
+
+            // Wait for that to complete.
+            control.instance.MainForm.EndInvoke(creation);
+        }
+
+        internal void SetTitle(string n)
+        {
+            regionName = n;
+            sessionForm.SetTitle("Voice: " + regionName);
+        }
+
+        /// <summary>
+        /// Close this session.
+        /// </summary>
+        internal void Close()
+        {
+            control.instance.MainForm.BeginInvoke(new MethodInvoker(delegate()
+            {
+                // Close the visible form
+                sessionForm.Hide();
+
+                // Remove the session from the contexts.
+                control.vClient.CloseSession(SessionHandle);
+            }));
+        }
+
+        internal void OnParticipantUpdate(string URI,
+            bool isMuted,
+            bool isSpeaking,
+            int volume,
+            float energy)
+        {
+            VoiceParticipant p = FindParticipant(URI);
+            sessionForm.SetParticipant(p.AvatarName, isMuted, isSpeaking, energy);
+        }
+
+        internal void AddParticipant(string URI)
+        {
+            VoiceParticipant p = FindParticipant(URI);
+
+            if (p.AvatarName == control.instance.Client.Self.Name)
+                control.vClient.PosUpdating(true);
+        }
+
+        internal void RemoveParticipant(string URI)
+        {
+            if (!knownParticipants.ContainsKey(URI))
+                return;
+            VoiceParticipant p = knownParticipants[URI];
+            sessionForm.RemoveParticipant(p.AvatarName);
+
+            if (p.AvatarName == control.instance.Client.Self.Name)
+                control.vClient.PosUpdating(false);
+        }
+
+        private VoiceParticipant FindParticipant(string puri)
+        {
+            VoiceParticipant p;
+
+            if (knownParticipants.ContainsKey(puri))
+            {
+                p = knownParticipants[puri];
+                if (p.AvatarName.StartsWith("Loading..."))
+                    p.AvatarName = control.instance.getAvatarName(p.id);
+                return p;
+            }
+
+
+            lock (knownParticipants)
+            {
+                p = new VoiceParticipant(puri);
+                knownParticipants.Add(puri, p);
+                p.AvatarName = control.instance.getAvatarName(p.id);
+                sessionForm.AddParticipant(p.AvatarName);
+            }
+
+            return p;
+        }
+
+
+    }
+}
---
+++ plugins/Radegast.Plugin.Voice/SessionForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Radegast.Plugin.Voice
+{
+    internal partial class SessionForm : Form
+    {
+        private enum State {
+            Idle = 0,
+            Talking,
+            Muted };
+
+        private Session session;
+        private VoiceControl control;
+
+        internal SessionForm( VoiceControl pc, Session s)
+        {
+            control = pc;
+            session = s;
+
+            InitializeComponent();
+        }
+
+        internal void AddParticipant( string name )
+        {
+            if (name == "") name = "?";
+
+            this.BeginInvoke(new MethodInvoker(delegate()
+            {
+                ListViewItem item = new ListViewItem(name);
+                item.StateImageIndex = (int)State.Idle;
+                item.Name = name;
+                participants.Items.Add(item);
+            }));
+        }
+
+        internal void RemoveParticipant(string name)
+        {
+            this.BeginInvoke(new MethodInvoker(delegate()
+            {
+                foreach (ListViewItem item in participants.Items)
+                {
+                    if (item.Name == name)
+                    {
+                        participants.Items.Remove(item);
+                        return;
+                    }
+                }
+            }));
+
+        }
+
+        internal void SetTitle(string t)
+        {
+            this.BeginInvoke(new MethodInvoker(delegate()
+            {
+                this.Text = t;
+            }));
+        }
+
+        private int FindItem(string name)
+        {
+            for (int n = 0; n < participants.Items.Count; n++)
+            {
+                ListViewItem item = participants.Items[n];
+                if (item.Name == name) return n;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Indicate who is speaking
+        /// </summary>
+        /// <param name="name">Avatar name</param>
+        /// <param name="isSpeaking"></param>
+        internal void SetParticipant(string name, bool isMuted, bool isSpeaking, float energy)
+        {
+            this.BeginInvoke(new MethodInvoker(delegate()
+            {
+                int n = FindItem(name);
+                if (n < 0) return;
+
+                ListViewItem item = participants.Items[n];
+
+                // Set icon depending on what they are doing
+                if (isMuted)
+                    item.StateImageIndex = (int)State.Muted;
+                else if (isSpeaking)
+                    item.StateImageIndex = (int)State.Talking;
+                else
+                    item.StateImageIndex = (int)State.Idle;
+            }));
+        }
+
+        private void SessionForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            session.Close();
+        }
+
+        private void SessionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Give user a chance to cancel session closing.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SessionForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r = MessageBox.Show(
+                "Do you mean to close this Voice session?",
+                "Really?",
+                MessageBoxButtons.YesNo );
+
+            if (r == DialogResult.No)
+                e.Cancel = true;
+        }
+
+ 
+    }
+}
