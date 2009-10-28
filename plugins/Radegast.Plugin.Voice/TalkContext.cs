1611d0ea063939b406d4ba0ad0c23e1f1e0d10f7
Mojito Sorbet <mojitotech@gmail.com>
2009-10-28 20:21:21 +0000

    Start of voice control menu
    git-svn-id: https://radegast.googlecode.com/svn/trunk@388 f7a694da-4d33-11de-9ad6-1127a62b9fcd

---
+++ plugins/Radegast.Plugin.Voice/TalkContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Radegast;
+
+namespace Radegast.Plugin.Voice
+{
+    class TalkContext
+    {
+        private RadegastContextMenuStrip menuStrip;
+
+        internal TalkContext()
+        {
+            RadegastContextMenuStrip.OnContentMenuItemClicked += OnContentMenuItemClicked;
+        }
+
+        internal void Start()
+        {
+            menuStrip = null;
+        }
+
+        internal void Stop()
+        {
+            menuStrip = null;
+        }
+
+        internal void SetObjectName(string name)
+        {
+        }
+
+        private void OnContentMenuItemClicked(object sender, RadegastContextMenuStrip.ContextMenuEventArgs e)
+        {
+            ListViewItem item = sender as ListViewItem;
+            if (item.Tag is Participant)
+            {
+                Participant p = item.Tag as Participant;
+                switch (e.MenuItem.Text)
+                {
+                    case "Mute":
+                        p.IsMuted = true;
+                        break;
+                    case "Unmute":
+                        p.IsMuted = false;
+                        break;
+                    case "Volume":
+                        // TODO Volume
+                        break;
+                }
+            }
+        }
+    }
+}
