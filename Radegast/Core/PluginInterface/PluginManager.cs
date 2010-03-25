de5739dbf6348daa76de0e679a2f53d556064b26
Latif Khalifa <latifer@streamgrid.net>
2010-03-25 07:02:07 +0000

    Cleanup usings
    git-svn-id: https://radegast.googlecode.com/svn/trunk@549 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/Core/PluginInterface/PluginManager.cs
+++ Radegast/Core/PluginInterface/PluginManager.cs
@@ -30,10 +30,7 @@
 //
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
-using System.Threading;
 using System.Reflection;
 using System.CodeDom.Compiler;
 using System.Windows.Forms;
