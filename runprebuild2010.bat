c117e874842e6b773084ca9f39ab8ae9b03c9fdc
Latif Khalifa <latifer@streamgrid.net>
2010-08-05 02:34:23 +0000

    Added auto dist building for TeamCity
    git-svn-id: https://radegast.googlecode.com/svn/trunk@721 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- runprebuild2010.bat
+++ runprebuild2010.bat
@@ -1,6 +1,6 @@
 @echo off
 ::
-:: Prebuild generator for the Radegast Library
+:: Prebuild generator for the Radegast
 ::
 :: Command Line Options:
 :: (none)            - create solution/project files and create compile.bat file to build solution
@@ -26,7 +26,7 @@ Radegast\prebuild.exe /target vs2010
 
 :: build compile.bat file based on command line parameters
 echo @echo off > compile.bat
-if(.%1)==(.) echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Radegast.sln >> compile.bat
+if(.%1)==(.) echo %SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild Radegast.sln >> compile.bat
 
 if(.%1)==(.msbuild) echo echo ==== COMPILE BEGIN ==== >> compile.bat
 if(.%1)==(.msbuild) echo %SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Configuration=Release Radegast.sln >> compile.bat
@@ -46,12 +46,26 @@ if(.%2)==(.runtests) echo nunit-console bin\Radegast.Tests.dll /exclude:Network 
 
 if(.%2)==(.runtests) echo IF ERRORLEVEL 1 GOTO FAIL >> compile.bat
 
-:: nsis compiler needs to be in path
-if(.%3)==(.dist) echo echo ==== GENERATE DISTRIBUTION BEGIN ==== >> compile.bat
-if(.%3)==(.dist) echo makensis.exe /DPlatform=test docs\Radegast-installer.nsi >> compile.bat
-if(.%3)==(.dist) echo IF ERRORLEVEL 1 GOTO FAIL >> compile.bat
-if(.%3)==(.dist) echo 7z.exe a -tzip dist\Radegast-dist.zip @docs\distfiles.lst >> compile.bat
-if(.%3)==(.dist) echo IF ERRORLEVEL 1 GOTO FAIL >> compile.bat
+if not (.%2)==(.dist) goto NODIST
+echo echo ==== GENERATE DISTRIBUTION BEGIN ==== >> compile.bat
+copy Radegast\radegast.nsi bin
+echo del /q Radegast-*-installer.exe >> compile.bat
+echo del /q radegast-latest.zip >> compile.bat
+
+if not exist "%PROGRAMFILES%\NSIS\Unicode\makensis.exe" goto NOUNINSIS
+echo "%PROGRAMFILES%\NSIS\Unicode\makensis.exe" bin\radegast.nsi >> compile.bat
+goto NONSIS
+:NOUNINSIS
+
+if not exist "%PROGRAMFILES%\NSIS\makensis.exe" goto NONSIS
+echo "%PROGRAMFILES%\NSIS\makensis.exe" bin\radegast.nsi >> compile.bat
+:NONSIS
+
+echo cd bin >> compile.bat
+echo 7z.exe a -r -tzip ..\radegast-latest.zip *.* >> compile.bat
+echo cd .. >> compile.bat
+
+:NODIST
 
 echo :SUCCESS >> compile.bat
 echo echo Build Successful! >> compile.bat
