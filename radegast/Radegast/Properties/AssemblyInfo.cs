3c8ca429b30ed8af5443ea8367da616224d2ccc7
Latif Khalifa <latifer@streamgrid.net>
2010-10-08 13:09:45 +0000

    Update version number to 1.24
    git-svn-id: https://radegast.googlecode.com/svn/branches/1.24-dev@748 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- radegast/Radegast/Properties/AssemblyInfo.cs
+++ radegast/Radegast/Properties/AssemblyInfo.cs
@@ -59,5 +59,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.23.0.*")]
-[assembly: AssemblyFileVersion("1.23.0.0")]
+[assembly: AssemblyVersion("1.24.748.*")]
+[assembly: AssemblyFileVersion("1.24.748.0")]
