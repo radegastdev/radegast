b991c446596906d81d17e6608fad875b8c06dd6c
Latif Khalifa <latifer@streamgrid.net>
2011-08-06 23:20:31 +0000

    Revert changes to tabs console from r993, breaks overflow tabs. Noticed by Zen-El (zenmondo.wormser)
    git-svn-id: https://radegast.googlecode.com/svn/trunk@1070 f7a694da-4d33-11de-9ad6-1127a62b9fcd

--- Radegast/GUI/Consoles/TabsConsole.Designer.cs
+++ Radegast/GUI/Consoles/TabsConsole.Designer.cs
@@ -120,8 +120,6 @@ namespace Radegast
             // 
             // tstTabs
             // 
-            this.tstTabs.AllowItemReorder = true;
-            this.tstTabs.CanOverflow = false;
             this.tstTabs.ContextMenuStrip = this.ctxTabs;
             this.tstTabs.Dock = System.Windows.Forms.DockStyle.None;
             this.tstTabs.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
