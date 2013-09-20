ee331b27a92b7fdb1317a27a9ec6e1b6e3ad270a
Latif Khalifa <latifer@streamgrid.net>
2013-09-20 04:51:40 +0200

    New icons and classes

---
+++ Radegast/Core/Types/Win32.cs
@@ -0,0 +1,177 @@
+﻿// 
+// Radegast Metaverse Client
+// Copyright (c) 2009-2013, Radegast Development Team
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the application "Radegast", nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Radegast
+{
+    /// <summary>
+    /// Summary description for Win32.
+    /// </summary>
+    public static class Win32
+    {
+
+        #region Consts
+        public const int WM_USER = 0x400;
+        public const int WM_PAINT = 0xF;
+        public const int WM_KEYDOWN = 0x100;
+        public const int WM_KEYUP = 0x101;
+        public const int WM_CHAR = 0x102;
+
+        public const int EM_GETSCROLLPOS = (WM_USER + 221);
+        public const int EM_SETSCROLLPOS = (WM_USER + 222);
+
+        public const int VK_CONTROL = 0x11;
+        public const int VK_UP = 0x26;
+        public const int VK_DOWN = 0x28;
+        public const int VK_NUMLOCK = 0x90;
+
+        public const short KS_ON = 0x01;
+        public const short KS_KEYDOWN = 0x80;
+
+        public const int EM_GETCHARFORMAT = WM_USER + 58;
+        public const int EM_SETCHARFORMAT = WM_USER + 68;
+
+        public const int SCF_SELECTION = 0x0001;
+        public const int SCF_WORD = 0x0002;
+        public const int SCF_ALL = 0x0004;
+        #endregion
+
+        #region Structs
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CHARFORMAT2_STRUCT
+        {
+            public UInt32 cbSize;
+            public UInt32 dwMask;
+            public UInt32 dwEffects;
+            public Int32 yHeight;
+            public Int32 yOffset;
+            public Int32 crTextColor;
+            public byte bCharSet;
+            public byte bPitchAndFamily;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public char[] szFaceName;
+            public UInt16 wWeight;
+            public UInt16 sSpacing;
+            public int crBackColor; // Color.ToArgb() -> int
+            public int lcid;
+            public int dwReserved;
+            public Int16 sStyle;
+            public Int16 wKerning;
+            public byte bUnderlineType;
+            public byte bAnimation;
+            public byte bRevAuthor;
+            public byte bReserved1;
+        }
+        #endregion
+
+        #region CHARFORMAT2 Flags
+        public const UInt32 CFE_BOLD = 0x0001;
+        public const UInt32 CFE_ITALIC = 0x0002;
+        public const UInt32 CFE_UNDERLINE = 0x0004;
+        public const UInt32 CFE_STRIKEOUT = 0x0008;
+        public const UInt32 CFE_PROTECTED = 0x0010;
+        public const UInt32 CFE_LINK = 0x0020;
+        public const UInt32 CFE_AUTOCOLOR = 0x40000000;
+        public const UInt32 CFE_SUBSCRIPT = 0x00010000;		/* Superscript and subscript are */
+        public const UInt32 CFE_SUPERSCRIPT = 0x00020000;		/*  mutually exclusive			 */
+
+        public const int CFM_SMALLCAPS = 0x0040;			/* (*)	*/
+        public const int CFM_ALLCAPS = 0x0080;			/* Displayed by 3.0	*/
+        public const int CFM_HIDDEN = 0x0100;			/* Hidden by 3.0 */
+        public const int CFM_OUTLINE = 0x0200;			/* (*)	*/
+        public const int CFM_SHADOW = 0x0400;			/* (*)	*/
+        public const int CFM_EMBOSS = 0x0800;			/* (*)	*/
+        public const int CFM_IMPRINT = 0x1000;			/* (*)	*/
+        public const int CFM_DISABLED = 0x2000;
+        public const int CFM_REVISED = 0x4000;
+
+        public const int CFM_BACKCOLOR = 0x04000000;
+        public const int CFM_LCID = 0x02000000;
+        public const int CFM_UNDERLINETYPE = 0x00800000;		/* Many displayed by 3.0 */
+        public const int CFM_WEIGHT = 0x00400000;
+        public const int CFM_SPACING = 0x00200000;		/* Displayed by 3.0	*/
+        public const int CFM_KERNING = 0x00100000;		/* (*)	*/
+        public const int CFM_STYLE = 0x00080000;		/* (*)	*/
+        public const int CFM_ANIMATION = 0x00040000;		/* (*)	*/
+        public const int CFM_REVAUTHOR = 0x00008000;
+
+
+        public const UInt32 CFM_BOLD = 0x00000001;
+        public const UInt32 CFM_ITALIC = 0x00000002;
+        public const UInt32 CFM_UNDERLINE = 0x00000004;
+        public const UInt32 CFM_STRIKEOUT = 0x00000008;
+        public const UInt32 CFM_PROTECTED = 0x00000010;
+        public const UInt32 CFM_LINK = 0x00000020;
+        public const UInt32 CFM_SIZE = 0x80000000;
+        public const UInt32 CFM_COLOR = 0x40000000;
+        public const UInt32 CFM_FACE = 0x20000000;
+        public const UInt32 CFM_OFFSET = 0x10000000;
+        public const UInt32 CFM_CHARSET = 0x08000000;
+        public const UInt32 CFM_SUBSCRIPT = CFE_SUBSCRIPT | CFE_SUPERSCRIPT;
+        public const UInt32 CFM_SUPERSCRIPT = CFM_SUBSCRIPT;
+
+        public const byte CFU_UNDERLINENONE = 0x00000000;
+        public const byte CFU_UNDERLINE = 0x00000001;
+        public const byte CFU_UNDERLINEWORD = 0x00000002; /* (*) displayed as ordinary underline	*/
+        public const byte CFU_UNDERLINEDOUBLE = 0x00000003; /* (*) displayed as ordinary underline	*/
+        public const byte CFU_UNDERLINEDOTTED = 0x00000004;
+        public const byte CFU_UNDERLINEDASH = 0x00000005;
+        public const byte CFU_UNDERLINEDASHDOT = 0x00000006;
+        public const byte CFU_UNDERLINEDASHDOTDOT = 0x00000007;
+        public const byte CFU_UNDERLINEWAVE = 0x00000008;
+        public const byte CFU_UNDERLINETHICK = 0x00000009;
+        public const byte CFU_UNDERLINEHAIRLINE = 0x0000000A; /* (*) displayed as ordinary underline	*/
+
+        public const int LVM_SETITEMCOUNT = 4143;
+        public const int LVSICF_NOSCROLL = 2;
+        #endregion
+
+        #region Imported functions
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr IntPtr, int msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32")]
+        public static extern int SendMessage(IntPtr IntPtr, int wMsg, int wParam, IntPtr lParam);
+        [DllImport("user32")]
+        public static extern int PostMessage(IntPtr IntPtr, int wMsg, int wParam, int lParam);
+        [DllImport("user32")]
+        public static extern short GetKeyState(int nVirtKey);
+        [DllImport("user32")]
+        public static extern int LockWindowUpdate(IntPtr IntPtr);
+        #endregion
+    }
+}
---
+++ Radegast/GUI/Consoles/Inventory/InvTreeView.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenMetaverse;
+
+namespace Radegast
+{
+    public class InvTreeView : ListViewNoFlicker
+    {
+        public class NodeMeta
+        {
+            public class NodeData
+            {
+                public bool IsExpanded;
+            }
+            public Dictionary<int, NodeData> Data = new Dictionary<int, NodeData>(2);
+        }
+
+        public class Node
+        {
+            int Id;
+            public Node(int id)
+            {
+                Id = id;
+            }
+
+            public InventoryNode ONode;
+            public bool IsDir { get { return ONode.Data is InventoryFolder; } }
+            public bool IsExpanded
+            {
+                get
+                {
+                    if (ONode.Tag is NodeMeta)
+                    {
+                        var meta = ONode.Tag as NodeMeta;
+                        if (meta.Data.ContainsKey(Id))
+                        {
+                            return meta.Data[Id].IsExpanded;
+                        }
+                    }
+                    return false;
+                }
+                set
+                {
+                    if (ONode.Tag is NodeMeta)
+                    {
+                        var meta = ONode.Tag as NodeMeta;
+                        if (meta.Data.ContainsKey(Id))
+                        {
+                            meta.Data[Id].IsExpanded = value;
+                        }
+                        else
+                        {
+                            meta.Data[Id] = new NodeMeta.NodeData() { IsExpanded = value };
+                        }
+                    }
+                    else
+                    {
+                        var meta = new NodeMeta();
+                        meta.Data[Id] = new NodeMeta.NodeData() { IsExpanded = value };
+                        ONode.Tag = meta;
+                    }
+                }
+            }
+            public int Level;
+        }
+
+        public int ID;
+        public static InvTreeSorter Sorter;
+
+        RadegastInstance Instance;
+        GridClient Client { get { return Instance.Client; } }
+        Inventory Inv { get { return Client.Inventory.Store; } }
+        List<Node> Mapper = new List<Node>(64);
+        int IconWidth = 16;
+        static Image IcnPlus, IcnMinus;
+
+        public InvTreeView(RadegastInstance instance)
+            : base()
+        {
+            Instance = instance;
+
+            FullRowSelect = false;
+            CheckBoxes = false;
+            View = System.Windows.Forms.View.Details;
+            LabelEdit = true;
+            LabelWrap = false;
+            Columns.Clear();
+            Columns.Add("Inventory", Width - 30);
+            HeaderStyle = ColumnHeaderStyle.None;
+            OwnerDraw = true;
+            SmallImageList = frmMain.ResourceImages;
+            InitSorter();
+            InitIcons();
+            VirtualMode = true;
+
+            UpdateMapper();
+        }
+
+        protected new virtual int VirtualListSize
+        {
+            get
+            {
+                return base.VirtualListSize;
+            }
+
+            set
+            {
+                if (Instance.MonoRuntime)
+                {
+                    base.VirtualListSize = value;
+                    return;
+                }
+
+                if (value == this.VirtualListSize || value < 0) return; // Get around the 'private' marker on 'virtualListSize' field using reflection
+                if (virtualListSizeFieldInfo == null)
+                {
+                    virtualListSizeFieldInfo = typeof(ListView).GetField("virtualListSize", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    System.Diagnostics.Debug.Assert(virtualListSizeFieldInfo != null);
+                } // Set the base class private field so that it keeps on working
+                virtualListSizeFieldInfo.SetValue(this, value); // Send a raw message to change the virtual list size *without* changing the scroll position
+                if (this.IsHandleCreated && !this.DesignMode)
+                {
+                    Win32.SendMessage(this.Handle, Win32.LVM_SETITEMCOUNT, value, (IntPtr)Win32.LVSICF_NOSCROLL);
+                }
+            }
+        }
+
+        static private System.Reflection.FieldInfo virtualListSizeFieldInfo;
+
+        public void UpdateMapper()
+        {
+            BeginUpdate();
+            Mapper.Clear();
+            var inv = new Node(ID)
+            {
+                ONode = Inv.RootNode,
+                Level = 0
+            };
+            var lib = new Node(ID)
+            {
+                ONode = Inv.LibraryRootNode,
+                Level = 0
+            };
+            Mapper.Add(inv);
+            PrepareNodes(Mapper.Count - 1, inv.IsExpanded);
+            Mapper.Add(lib);
+            PrepareNodes(Mapper.Count - 1, lib.IsExpanded);
+            VirtualListSize = Mapper.Count;
+            EndUpdate();
+        }
+
+        void InitSorter()
+        {
+            if (Sorter == null)
+            {
+                Sorter = new InvTreeSorter();
+                if (!Instance.GlobalSettings.ContainsKey("inv_sort_bydate"))
+                    Instance.GlobalSettings["inv_sort_bydate"] = true;
+                if (!Instance.GlobalSettings.ContainsKey("inv_sort_sysfirst"))
+                    Instance.GlobalSettings["inv_sort_sysfirst"] = true;
+
+                Sorter.ByDate = Instance.GlobalSettings["inv_sort_bydate"];
+                Sorter.SystemFoldersFirst = Instance.GlobalSettings["inv_sort_sysfirst"];
+            }
+        }
+
+        void InitIcons()
+        {
+            if (IcnPlus == null)
+            {
+                IcnPlus = frmMain.ResourceImages.Images[frmMain.ImageNames.IndexOf("arrow_right")];
+                IcnMinus = frmMain.ResourceImages.Images[frmMain.ImageNames.IndexOf("arrow_down")];
+            }
+        }
+
+        void PrepareNodes(int pos, bool add)
+        {
+            Node mbr = Mapper[pos];
+            pos++;
+
+            if (add)
+            {
+                PopulateDescendantMembers(ref pos, mbr);
+            }
+            else
+            {
+                int kids = ObtainExpandedChildrenCount(pos - 1);
+                Mapper.RemoveRange(pos, kids);
+            }
+
+            VirtualListSize = Mapper.Count;
+        }
+
+        /// <summary>
+        /// Populates the descendant members.
+        /// </summary>
+        /// <param name="pos">The pos.</param>
+        /// <param name="mbr">The data node.</param>
+        private void PopulateDescendantMembers(ref int pos, Node mbr)
+        {
+            List<InventoryNode> children = new List<InventoryNode>(mbr.ONode.Nodes.Values);
+            children.Sort(Sorter);
+
+            foreach (var m in children)
+            {
+                Node newNode = new Node(ID)
+                {
+                    ONode = m,
+                    Level = mbr.Level + 1,
+                };
+
+                Mapper.Insert(pos++, newNode);
+                if (newNode.IsExpanded)
+                {
+                    PopulateDescendantMembers(ref pos, newNode);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Obtains the expanded children count.
+        /// </summary>
+        /// <param name="pos">The pos.</param>
+        /// <returns></returns>
+        private int ObtainExpandedChildrenCount(int pos)
+        {
+            int kids = 0;
+            Node mi = Mapper[pos];
+            int level = mi.Level;
+
+            for (int i = pos + 1; i < Mapper.Count; i++, kids++)
+            {
+                Node mix = Mapper[i];
+                int lvlx = mix.Level;
+                if (lvlx <= level) break;
+            }
+
+            return kids;
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ListViewItem item = GetItemAt(e.X, e.Y);
+                if (item != null)
+                {
+                    int xfrom = item.IndentCount * IconWidth;
+                    int xto = xfrom + IconWidth + 4;
+                    Node n = (Node)item.Tag;
+                    if (e.X >= xfrom && e.X < xto)
+                    {
+                        n.IsExpanded = !n.IsExpanded;
+                        item.Checked = !item.Checked;
+                        PrepareNodes(item.Index, n.IsExpanded);
+                        return;
+                    }
+                }
+            }
+
+            base.OnMouseClick(e);
+        }
+
+        protected override void OnRetrieveVirtualItem(RetrieveVirtualItemEventArgs e)
+        {
+            Node n;
+            try
+            {
+                n = Mapper[e.ItemIndex];
+            }
+            catch
+            {
+                e.Item = new ListViewItem();
+                return;
+            }
+            ListViewItem item = new ListViewItem(n.ONode.Data.Name);
+            var ix = InventoryConsole.GetIconIndex(n.ONode.Data);
+            //item.StateImageIndex = ix;
+            item.IndentCount = n.Level;
+            item.Tag = n;
+            e.Item = item;
+        }
+
+        protected override void OnDrawColumnHeader(DrawListViewColumnHeaderEventArgs e)
+        {
+            e.DrawDefault = true;
+            base.OnDrawColumnHeader(e);
+        }
+
+        protected override void OnDrawItem(DrawListViewItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            e.DrawBackground();
+
+            if (!(e.Item.Tag is Node))
+                return;
+
+            if (e.Item.Selected)
+            {
+                g.FillRectangle(SystemBrushes.Highlight, e.Bounds);
+            }
+
+            Node node = e.Item.Tag as Node;
+
+            Image icon = null;
+            int iconIx = InventoryConsole.GetIconIndex(node.ONode.Data);
+            int offset = (IconWidth + 2) * (node.Level + 2);
+            Rectangle rec = new Rectangle(e.Bounds.X + offset, e.Bounds.Y, e.Bounds.Width - offset, e.Bounds.Height);
+
+            try
+            {
+                icon = frmMain.ResourceImages.Images[iconIx];
+                g.DrawImageUnscaled(icon, e.Bounds.X + offset - IconWidth, e.Bounds.Y);
+                if (node.IsDir && node.ONode.Nodes.Count > 0)
+                {
+                    Image exp;
+                    if (node.IsExpanded)
+                    {
+                        exp = IcnMinus;
+                    }
+                    else
+                    {
+                        exp = IcnPlus;
+                    }
+                    g.DrawImageUnscaled(exp, e.Bounds.X + offset - (IconWidth + 2) * 2, e.Bounds.Y);
+                }
+
+            }
+            catch { }
+
+            using (StringFormat sf = new StringFormat(StringFormatFlags.NoWrap | StringFormatFlags.LineLimit))
+            {
+                string label = node.ONode.Data.Name; // ItemLabel(res.Inv, false);
+                SizeF len = e.Graphics.MeasureString(label, Font, rec.Width, sf);
+
+                e.Graphics.DrawString(
+                    label,
+                    Font,
+                    e.Item.Selected ? SystemBrushes.HighlightText : SystemBrushes.WindowText,
+                    rec,
+                    sf);
+
+            }
+            base.OnDrawItem(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            if (Columns.Count > 0)
+            {
+                Columns[0].Width = Width - 30;
+            }
+            base.OnResize(e);
+        }
+
+        #region Sorter class
+        // Create a node sorter that implements the IComparer interface.
+        public class InvTreeSorter : IComparer<InventoryNode>
+        {
+            bool _sysfirst = true;
+            bool _bydate = true;
+
+            int CompareFolders(InventoryFolder x, InventoryFolder y)
+            {
+                if (_sysfirst)
+                {
+                    if (x.PreferredType != AssetType.Unknown && y.PreferredType == AssetType.Unknown)
+                    {
+                        return -1;
+                    }
+                    else if (x.PreferredType == AssetType.Unknown && y.PreferredType != AssetType.Unknown)
+                    {
+                        return 1;
+                    }
+                }
+                return String.Compare(x.Name, y.Name);
+            }
+
+            public bool SystemFoldersFirst { set { _sysfirst = value; } get { return _sysfirst; } }
+            public bool ByDate { set { _bydate = value; } get { return _bydate; } }
+
+            /*
+            public int Compare(object x, object y)
+            {
+                InventoryNode tx = x as InventoryNode;
+                InventoryNode ty = y as InventoryNode;
+             */
+            public int Compare(InventoryNode tx, InventoryNode ty)
+            {
+                if (tx.Data is InventoryFolder && ty.Data is InventoryFolder)
+                {
+                    return CompareFolders(tx.Data as InventoryFolder, ty.Data as InventoryFolder);
+                }
+                else if (tx.Data is InventoryFolder && ty.Data is InventoryItem)
+                {
+                    return -1;
+                }
+                else if (tx.Data is InventoryItem && ty.Data is InventoryFolder)
+                {
+                    return 1;
+                }
+
+                // Two items
+                if (!(tx.Data is InventoryItem) || !(ty.Data is InventoryItem))
+                {
+                    return 0;
+                }
+
+                InventoryItem item1 = (InventoryItem)tx.Data;
+                InventoryItem item2 = (InventoryItem)ty.Data;
+
+                if (_bydate)
+                {
+                    if (item1.CreationDate < item2.CreationDate)
+                    {
+                        return 1;
+                    }
+                    else if (item1.CreationDate > item2.CreationDate)
+                    {
+                        return -1;
+                    }
+                }
+                return string.Compare(item1.Name, item2.Name);
+            }
+        }
+        #endregion
+    }
+}
