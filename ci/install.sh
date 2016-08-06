f78550002d1dc4311e42ae5fd1524934cba555cd
nooperation <madbrahmin@gmail.com>
2016-08-06 00:04:15 -0400

    Updating ci install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo '// Placeholder for automated build systems to implement their build number.' > RadegastBuild.cs
-echo 'namespace Radegast ' >> RadegastBuild.cs
-echo '{ ' >> RadegastBuild.cs
-echo '    public static class RadegastBuild ' >> RadegastBuild.cs
-echo '    {' >> RadegastBuild.cs
-echo '        public const string VersionString = "'$BUILD_MAJOR'.'$BUILD_MINOR'.'$TRAVIS_BUILD_NUMBER'";' >> RadegastBuild.cs
-echo '        public const string BuildName = "'$GIT_TAG'";' >> RadegastBuild.cs
-echo '    }' >> RadegastBuild.cs
-echo '}' >> RadegastBuild.cs
+echo '// Placeholder for automated build systems to implement their build number.' > Radegast/RadegastBuild.cs
+echo 'namespace Radegast ' >> Radegast/RadegastBuild.cs
+echo '{ ' >> Radegast/RadegastBuild.cs
+echo '    public static class RadegastBuild ' >> Radegast/RadegastBuild.cs
+echo '    {' >> Radegast/RadegastBuild.cs
+echo '        public const string VersionString = "'$BUILD_MAJOR'.'$BUILD_MINOR'.'$TRAVIS_BUILD_NUMBER'";' >> Radegast/RadegastBuild.cs
+echo '        public const string BuildName = "'$GIT_TAG'";' >> Radegast/RadegastBuild.cs
+echo '    }' >> Radegast/RadegastBuild.cs
+echo '}' >> Radegast/RadegastBuild.cs
 ./runprebuild.sh
