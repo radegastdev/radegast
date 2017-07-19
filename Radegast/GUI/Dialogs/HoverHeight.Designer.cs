aef5e1256384840284b35cb11e6b26a7eea1c2aa
nooperation <madbrahmin@gmail.com>
2017-07-18 23:59:15 -0400

    Added hover height dialog

---
+++ Radegast/GUI/Dialogs/HoverHeight.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Radegast
+{
+    partial class frmHoverHeight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbHoverHeight = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoverHeight = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHoverHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbHoverHeight
+            // 
+            this.tbHoverHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+                                                                              | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbHoverHeight.Location = new System.Drawing.Point(56, 9);
+            this.tbHoverHeight.Maximum = 200;
+            this.tbHoverHeight.Minimum = -200;
+            this.tbHoverHeight.Name = "tbHoverHeight";
+            this.tbHoverHeight.Size = new System.Drawing.Size(290, 45);
+            this.tbHoverHeight.TabIndex = 0;
+            this.tbHoverHeight.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.tbHoverHeight.Scroll += new System.EventHandler(this.tbHoverHeight_Scroll);
+            this.tbHoverHeight.MouseCaptureChanged += new System.EventHandler(this.tbHoverHeight_MouseCaptureChanged);
+            this.tbHoverHeight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tbHoverHeight_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Height";
+            // 
+            // txtHoverHeight
+            // 
+            this.txtHoverHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHoverHeight.Location = new System.Drawing.Point(353, 9);
+            this.txtHoverHeight.Name = "txtHoverHeight";
+            this.txtHoverHeight.Size = new System.Drawing.Size(68, 21);
+            this.txtHoverHeight.TabIndex = 2;
+            this.txtHoverHeight.Tag = "";
+            // 
+            // frmHoverHeight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 43);
+            this.Controls.Add(this.txtHoverHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHoverHeight);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmHoverHeight";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Hover Height";
+            ((System.ComponentModel.ISupportInitialize)(this.tbHoverHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbHoverHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoverHeight;
+    }
+}
---
+++ Radegast/GUI/Dialogs/HoverHeight.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace Radegast
+{
+    public partial class frmHoverHeight : Form, INotifyPropertyChanged
+    {
+        private const int HoverHeightPrecision = 100;
+
+        private bool IsMouseDown { get; set; }
+        private bool IsScrolling { get; set; }
+
+        private double _realHoverHeight;
+        public double RealHoverHeight
+        {
+            get => _realHoverHeight;
+            set
+            {
+                if (Math.Abs(RealHoverHeight - value) > double.Epsilon)
+                {
+                    _realHoverHeight = value;
+                    OnPropertyChanged();
+
+                    if (!IsScrolling)
+                    {
+                        OnHoverHeightChanged(_realHoverHeight);
+                    }
+                    else
+                    {
+                        OnHoverHeightPreview(_realHoverHeight);
+                    }
+                }
+            }
+        }
+
+        public int FakeHoverHeight
+        {
+            get => (int)(RealHoverHeight * HoverHeightPrecision);
+            set
+            {
+                if (FakeHoverHeight != value)
+                {
+                    RealHoverHeight = value / (double) HoverHeightPrecision;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public frmHoverHeight(double initialHoverHeight)
+        {
+            InitializeComponent();
+
+            tbHoverHeight.Maximum = 2 * HoverHeightPrecision;
+            tbHoverHeight.Minimum = -2 * HoverHeightPrecision;
+            tbHoverHeight.DataBindings.Add(new Binding("Value", this, nameof(FakeHoverHeight), true, DataSourceUpdateMode.OnPropertyChanged));
+
+            var textBinding = new Binding("Text", this, nameof(RealHoverHeight), true, DataSourceUpdateMode.OnPropertyChanged);
+            textBinding.Parse += textBinding_Parse;
+            txtHoverHeight.DataBindings.Add(textBinding);
+
+            RealHoverHeight = initialHoverHeight;
+        }
+
+        private void textBinding_Parse(object sender, ConvertEventArgs e)
+        {
+            var sourceBinding = sender as Binding;
+            var sourceControl = sourceBinding.Control as TextBox;
+
+            var attemptedValue = 0.0;
+            if (double.TryParse(sourceControl.Text, out attemptedValue))
+            {
+                sourceControl.ForeColor = TextBox.DefaultForeColor;
+                if (attemptedValue < -2.0)
+                {
+                    e.Value = -2.0;
+                    sourceControl.Text = e.Value.ToString();
+                }
+                else if(attemptedValue > 2.0)
+                {
+                    e.Value = 2.0;
+                    sourceControl.Text = e.Value.ToString();
+                }
+            }
+            else
+            {
+                sourceControl.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void tbHoverHeight_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            if (IsMouseDown)
+            {
+                IsMouseDown = false;
+            }
+
+            if (IsScrolling)
+            {
+                IsScrolling = false;
+                OnHoverHeightChanged(RealHoverHeight);
+            }
+        }
+
+        private void tbHoverHeight_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = true;
+        }
+
+        private void tbHoverHeight_Scroll(object sender, EventArgs e)
+        {
+            if (IsMouseDown)
+            {
+                IsScrolling = true;
+            }
+        }
+
+        public event EventHandler<HoverHeightChangedEventArgs> HoverHeightChanged;
+        void OnHoverHeightChanged(double newHoverHeight)
+        {
+            HoverHeightChanged?.Invoke(this, new HoverHeightChangedEventArgs(newHoverHeight));
+        }
+
+        public event EventHandler<HoverHeightChangedEventArgs> HoverHeightPreview;
+        void OnHoverHeightPreview(double newHoverHeight)
+        {
+            HoverHeightPreview?.Invoke(this, new HoverHeightChangedEventArgs(newHoverHeight));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class HoverHeightChangedEventArgs : EventArgs
+    {
+        public double HoverHeight { get; set; }
+
+        public HoverHeightChangedEventArgs(double newHoverHeight)
+        {
+            HoverHeight = newHoverHeight;
+        }
+    }
+}
