339165c6d25398bc5fce878c4f6994892d8c55c1
Cinder <cinder@cinderblocks.biz>
2021-06-23 11:22:16 -0500

    Convert ContextAction Async thread to a Task

--- Radegast/Core/Contexts/IContextAction.cs
+++ Radegast/Core/Contexts/IContextAction.cs
@@ -27,6 +27,7 @@ namespace Radegast
     public interface IContextAction : IDisposable
     {
         void IContextAction(RadegastInstance instance);
+
         /// <summary>
         /// Generate a list of ToolStripMenuItems that might be be embeded into a ContextMenuStrip host
         /// </summary>
@@ -34,25 +35,21 @@ namespace Radegast
         /// <param name="type">the dereferenced type</param>
         /// <returns>List of ToolStripMenuItem that will be appeneded to the current menu</returns>
         IEnumerable<ToolStripMenuItem> GetToolItems(object target, Type type);
-        /// <summary>
-        /// Get GUI items that one might include on the form to operate this action
-        /// </summary>
-        /// <param name="target">the context sensitive item</param>
-        /// <param name="type">the dereferenced type</param>
-        /// <returns>List of Buttons and Other hostable Controls that make sense to appear on the form when this item is selected</returns>
-        IEnumerable<Control> GetControls(object target, Type type);
+
         /// <summary>
         /// If the menu item is Enabled
         /// </summary>
         /// <param name="target">the context sensiive item</param>
         /// <returns>false when the menu Item should be greyed</returns>
         bool IsEnabled(object target);
+
         /// <summary>
         /// The menu Item's text based on target 
         /// </summary>
         /// <param name="target">the context sensiive item</param>
         /// <returns>true if the Action is availble - for instance some Avatar might not even be logged in so "follow" would not even show up</returns>
         string LabelFor(object target);
+
         /// <summary>
         /// If the context menu is usable to the target
         /// </summary>
@@ -60,6 +57,7 @@ namespace Radegast
         /// <param name="type">the dereferenced type</param>
         /// <returns>The name that is displayed in a menu of options</returns>
         bool Contributes(Object target, Type type);
+
         /// <summary>
         /// The Action code goes here
         /// </summary>
